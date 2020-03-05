74af5c277ac4b640e3f37436151071fc26610cef
Alefy Pimentel <alefy.p.ferreira@gmail.com>
2020-03-05 17:37:43 -0300

    update

--- src/index.js
+++ src/index.js
@@ -1,34 +1,34 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import './style.css';
-import Timer from './components/timer';
-import Period from './components/period';
-import Music from './components/music';
-import Score from './components/score';
-import * as serviceWorker from './serviceWorker';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import "./style.css";
+import Timer from "./components/timer";
+import Period from "./components/period";
+import Music from "./components/music";
+import Score from "./components/score";
+import * as serviceWorker from "./serviceWorker";
 
 class App extends Component {
-    render() {
-        return (
-            <div>
-                <Timer start="t" />
-                <Period addPeriod="[" removePeriod="]" />
-                <Score
-                    teamNameA="TIME Alefy"
-                    teamNameB="TIME DANIEL"
-                    addA="a"
-                    removeA="s"
-                    addB="k"
-                    removeB="l"
-                />
-                <Music />
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <Timer start="t" />
+        <Period addPeriod="[" removePeriod="]" />
+        <Score
+          teamNameA="TIME Alefy"
+          teamNameB="TIME DANIEL"
+          addA="a"
+          removeA="s"
+          addB="k"
+          removeB="l"
+        />
+        <Music />
+      </div>
+    );
+  }
 }
 
 export default App;
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 serviceWorker.unregister();
